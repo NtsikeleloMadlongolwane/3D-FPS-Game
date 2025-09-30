f64783ff5674310d0d3304067ebe4b7272976d91
NtsikeleloMadlongolwane <2154536@students.wits.ac.za>
2025-09-30 20:34:06 +0200

    DragNSpinWorks

---
+++ First Person Game  2025/Assets/Scripts/UI scripts/MousePision.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class MousePision : MonoBehaviour
+{
+    [SerializeField]
+    private bool isInDragArea = false;
+    private Vector3 mousePosition;
+
+    public Transform topBound;
+    public Transform bottomBound;
+    public Transform leftBound;
+    public Transform rightBound;
+    void Update()
+    {
+        Vector3 top = Camera.main.WorldToScreenPoint(topBound.position);
+        Vector3 bottom = Camera.main.WorldToScreenPoint(bottomBound.position);
+        Vector3 left = Camera.main.WorldToScreenPoint(leftBound.position);
+        Vector3 right = Camera.main.WorldToScreenPoint(rightBound.position);
+
+        mousePosition = Input.mousePosition;
+        if ((mousePosition.y < top.y && mousePosition.y > bottom.y) 
+            && (mousePosition.x < right.x && mousePosition.x > left.x))
+        {
+            isInDragArea = true;
+        }
+        else
+        {
+            isInDragArea = false;
+        }
+    }
+}
