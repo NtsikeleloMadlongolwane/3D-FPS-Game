2b8885cae8011b9b6bc7fd91e4e65b71c3601922
NtsikeleloMadlongolwane <2154536@students.wits.ac.za>
2025-10-19 07:50:15 +0200

    UI done

--- First Person Game  2025/Assets/Scripts/Scripts 2/FPController.cs
+++ First Person Game  2025/Assets/Scripts/Scripts 2/FPController.cs
@@ -211,10 +211,10 @@ public class FPController : MonoBehaviour
             Destroy(bullet, 3f);
         } // shooting
 
-       /* if (canSwich)
+       if (canSwich)
         {
             TogoClap();
-        } */
+        }
        
         // todo gun
 
@@ -330,7 +330,7 @@ public class FPController : MonoBehaviour
             }
         }
     }
-    public void TogoClap(InputAction.CallbackContext context)
+    public void TogoClap()
     {
         if (!canSwich) return;
         if (!canMove) return;
@@ -368,6 +368,7 @@ public class FPController : MonoBehaviour
     }
     public void Dash()
     {
+        if (!canMove) return;
         if (dashTime > 0)
         {
             Vector3 dashDirection = transform.forward;
@@ -382,7 +383,7 @@ public class FPController : MonoBehaviour
 
     public void PauseGame(InputAction.CallbackContext context)
     {
-        if (!canMove) return;
+       // if (!canMove) return;
         HandlePause();      
     }
 
@@ -400,6 +401,7 @@ public class FPController : MonoBehaviour
             Cursor.visible = true;
 
             mMR.PauseMenu();
+            mMR.ClearPauseMenu();
         }
         else
         {
@@ -413,6 +415,7 @@ public class FPController : MonoBehaviour
             Cursor.visible = true;
 
             mMR.PauseMenu();
+            mMR.ClearPauseMenu();
         }
 
     }
