ecd511970406419d182a3fa9dea23ed8912c9e24
NtsikeleloMadlongolwane <2154536@students.wits.ac.za>
2025-11-05 21:33:50 +0200

    mountain fixed

--- First Person Game  2025/Assets/Scripts/Platformi Scripts/Destroyer.cs
+++ First Person Game  2025/Assets/Scripts/Platformi Scripts/Destroyer.cs
@@ -5,9 +5,10 @@ public class Destroyer : MonoBehaviour
     public string targetTag;
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag(tag))
+        if (other.gameObject.CompareTag(targetTag))
         {
             Destroy(other.gameObject);
+            Debug.Log("Object Destroyed");
         }
     }
 }
